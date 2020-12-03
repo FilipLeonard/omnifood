3e3cec6f9af888d310f8e5383fb974f5f0d3a20e
FilipLeonard <leonard.filip.93@gmail.com>
2020-12-03 18:41:10 +0200

    adds hamburger menu for mobile navigation

--- resources/js/script.js
+++ resources/js/script.js
@@ -37,4 +37,37 @@ function main() {
     },
     { offset: '50%' }
   );
+
+  $('.js--wp-2').waypoint(
+    function (direction) {
+      $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+    },
+    { offset: '50%' }
+  );
+  $('.js--wp-3').waypoint(
+    function (direction) {
+      $('.js--wp-3').addClass('animate__animated animate__fadeInUp');
+    },
+    { offset: '50%' }
+  );
+  $('.js--wp-4').waypoint(
+    function (direction) {
+      $('.js--wp-4').addClass('animate__animated animate__pulse');
+    },
+    { offset: '50%' }
+  );
+
+  // Mobile navigation
+
+  $('.js--mobile-nav-icon').click(function () {
+    $('.main-nav').slideToggle(300);
+    $('.js--mobile-nav-icon-close').removeClass('hidden');
+    $('.js--mobile-nav-icon').css('display', 'none');
+  });
+
+  $('.js--mobile-nav-icon-close').click(function () {
+    $('.main-nav').slideToggle(300);
+    $('.js--mobile-nav-icon-close').addClass('hidden');
+    $('.js--mobile-nav-icon').css('display', 'block');
+  });
 }
